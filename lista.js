f3426c364713f6a5ae4860107eb4f9a757c10b49
hiury <hiuryh2k@gmail.com>
2019-08-21 16:58:43 -0300

    Servidor Rodando com unico filtro area_conhecimento

--- lista.js
+++ lista.js
@@ -2,16 +2,16 @@ const url = 'https://dados.ifpb.edu.br/dataset/e99b5cfd-f2f3-4b54-bb4f-6ddd9e480
 const axios = require('axios')
 
 const area = f => f.area_conhecimento === 'ENGENHARIA CIVIL' // filtar por contador
-const resumo = f => f.resumo === 'Modalidade local'
-const justificativa = f => f.justificativa === 'Nada consta' // O IGUAL E EXATAMENTE IGUAL  FILTRA APOSENTADO
+//const resumo = f => f.resumo === 'Modalidade local'
+//const justificativa = f => f.justificativa === 'Nada consta' // O IGUAL E EXATAMENTE IGUAL  FILTRA APOSENTADO
 
 
 
 axios.get(url).then(resposta => {
     const servidores = resposta.data
     module.exports.resultado =
-    servidores.filter(area).filter(resumo).filter(justificativa)
+    servidores.filter(area)//filter(resumo).filter(justificativa)
 
-    const resultado_filtro = servidores.filter(area).filter(resumo).filter(justificativa)
-    console.log(resultado_filtro)
+    // const resultado_filtro = servidores.filter(area)//.filter(resumo).filter(justificativa)
+    // console.log(resultado_filtro)
 })
